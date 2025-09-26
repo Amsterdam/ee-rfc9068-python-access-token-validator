8d7596bf3168440b87d84a482a558ba5f54a90f7
Youri Westerman <y.westerman@amsterdam.nl>
2025-09-26 13:46:01 +0200

    Test behavior when JWK client does not return the right key type

--- tests/signature/test_signature_validator.py
+++ tests/signature/test_signature_validator.py
@@ -1,14 +1,14 @@
 from unittest.mock import Mock
 
 import pytest
-from jwt import InvalidSignatureError as PyJWTInvalidSignatureError
+from jwt import InvalidSignatureError as PyJWTInvalidSignatureError, PyJWK, PyJWKClient
 from jwt import PyJWS
 
 from rfc9068.parser import JWTHeader, ValidAlgHeaderValues, ValidTypHeaderValues
 from rfc9068.signature import (
     InvalidSignatureError,
     JWKResolverInterface,
-    PyJwtSignatureValidator,
+    PyJwtSignatureValidator, PyJwtJWKResolver,
 )
 
 
@@ -19,6 +19,28 @@ def test_raises_when_signature_invalid() -> None:
     validate = PyJwtSignatureValidator(Mock(JWKResolverInterface), py_jws)
 
     with pytest.raises(InvalidSignatureError):
+        validate(
+            JWTHeader(
+                alg=ValidAlgHeaderValues.RS256,
+                typ=ValidTypHeaderValues.AT_JWT,
+                kid="kid1",
+            ),
+            '{"alg":"RS256","typ":"at+jwt","kid":"kid1"}',
+            '{"iss": "test_issuer","exp": 1234,"aud": ["aud"],'
+            '"sub": "sub","client_id": "client_id","iat": 5678,"jti": "jti",}',
+            b"veryprettyfakesignature",
+            ["RS256"],
+        )
+
+
+def test_raises_when_key_is_not_public_rsa_key() -> None:
+    jwk = PyJWK({"kty": "oct", "k": ""})
+    jwk_client = Mock(PyJWKClient)
+    jwk_client.get_signing_key.return_value = jwk
+    jwk_resolver = PyJwtJWKResolver(jwk_client)
+
+    validate = PyJwtSignatureValidator(jwk_resolver, Mock(PyJWS))
+    with pytest.raises(TypeError):
         validate(
             JWTHeader(
                 alg=ValidAlgHeaderValues.RS256,
